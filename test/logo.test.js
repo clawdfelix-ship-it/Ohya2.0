63ee5735eaeef1d9c819c0cb75c96dd8430d1ef1
Chan Siu Lung Felix <chansiulungfelix@ChandeMac-mini.local>
2026-04-28 01:33:19 +0800

    feat: set homepage logo to ohya2.0

---
+++ test/logo.test.js
@@ -0,0 +1,9 @@
+const test = require('node:test');
+const assert = require('node:assert/strict');
+const fs = require('node:fs');
+const path = require('node:path');
+
+test('homepage top-left logo text is OHYA2.0', () => {
+  const content = fs.readFileSync(path.join(__dirname, '..', 'views', 'index.ejs'), 'utf8');
+  assert.ok(content.includes('OHYA2.0'), 'index.ejs should contain OHYA2.0 logo text');
+});
